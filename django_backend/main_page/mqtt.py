ba43c55e626f8853fc840c214edd702c03a4c2fc
Jeacson_Snake <Jeacson_Snake@outlook.com>
2025-10-06 13:22:00 +0800

    fix: fix issue when MQTT not connected
    using try-catch to held the MQTT-connecting problem (the problem occured while running backend code
    without a MQTT broker)

--- django_backend/main_page/mqtt.py
+++ django_backend/main_page/mqtt.py
@@ -177,8 +177,11 @@ if os.environ.get('RUN_MAIN'):
     client.on_connect = on_connect
     client.on_message = on_message
     client.username_pw_set(settings.MQTT_USER, settings.MQTT_PASSWORD)
-    client.connect(
-        host=settings.MQTT_SERVER,
-        port=settings.MQTT_PORT,
-        keepalive=settings.MQTT_KEEPALIVE
-    )
+    try:
+        client.connect(
+            host=settings.MQTT_SERVER,
+            port=settings.MQTT_PORT,
+            keepalive=settings.MQTT_KEEPALIVE
+        )
+    except Exception as e:
+        print("MQTT Connect Failed! Error: ", e)
